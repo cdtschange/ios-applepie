bb90f879cb4c3aa8638c1064de44e1abd68ebd22
wei.mao <wei.mao@pintec.com>
2018-10-26 16:29:51 +0800

    init

---
+++ Applepie/Extension/Applepie.swift
@@ -0,0 +1,61 @@
+//
+//  Applepie.swift
+//  Applepie
+//
+//  Created by 毛蔚 on 2018/10/26.
+//  Copyright © 2018 毛蔚. All rights reserved.
+//
+
+import Foundation
+import ImageIO
+
+#if os(macOS)
+import AppKit
+public typealias Image = NSImage
+public typealias View = NSView
+public typealias Color = NSColor
+public typealias ImageView = NSImageView
+public typealias Button = NSButton
+#else
+import UIKit
+public typealias Image = UIImage
+public typealias Color = UIColor
+#if !os(watchOS)
+public typealias ImageView = UIImageView
+public typealias View = UIView
+public typealias Button = UIButton
+#else
+import WatchKit
+#endif
+#endif
+
+public final class Applepie<Base> {
+    public let base: Base
+    public init(_ base: Base) {
+        self.base = base
+    }
+}
+
+/**
+ A type that has Applepie extensions.
+ */
+public protocol ApplepieCompatible {
+    associatedtype CompatibleType
+    var ap: CompatibleType { get }
+}
+
+public extension ApplepieCompatible {
+    public var ap: Applepie<Self> {
+        return Applepie(self)
+    }
+}
+
+extension Image: ApplepieCompatible { }
+extension String: ApplepieCompatible { }
+extension Data: ApplepieCompatible { }
+#if !os(watchOS)
+extension ImageView: ApplepieCompatible { }
+extension Button: ApplepieCompatible { }
+#else
+extension WKInterfaceImage: KingfisherCompatible { }
+#endif
