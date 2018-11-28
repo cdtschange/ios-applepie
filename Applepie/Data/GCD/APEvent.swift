8211903b51ebde6a1c4b4d167c70a9df61b065f5
wei.mao <wei.mao@pintec.com>
2018-11-28 09:53:12 +0800

    api

--- Applepie/Data/GCD/APEvent.swift
+++ Applepie/Data/GCD/APEvent.swift
@@ -8,10 +8,26 @@
 
 import Foundation
 
+
 public struct APEvent {
+    public typealias APEventBlock = () -> ()
+    private var queue: DispatchQueue
+    private var group: DispatchGroup?
+    private var blocks: [APEventBlock] = []
     
-    /// 主线程需要子线程的处理结果
-    public static func execute<T>(queue: DispatchQueue = DispatchQueue.global(), waitFor: @escaping () -> T, finshed: @escaping (T) -> ()) {
+    public init() {
+        queue = DispatchQueue.global()
+    }
+    public init(queue: DispatchQueue) {
+        self.queue = queue
+    }
+    public mutating func setQueue(queue: DispatchQueue) -> APEvent {
+        self.queue = queue
+        return self
+    }
+    
+    /// 执行一个耗时操作后回调主线程，主线程需要子线程的处理结果
+    public func execute<T>(waitFor: @escaping () -> T, finshed: @escaping (T) -> ()) {
         queue.async {
             let data = waitFor()
             DispatchQueue.main.async {
@@ -20,8 +36,8 @@ public struct APEvent {
         }
     }
     
-    /// 主线程不需要子线程的处理结果
-    public static func execute(queue: DispatchQueue = DispatchQueue.global(), waitFor: @escaping () -> (), finshed: @escaping () -> ()) {
+    /// 执行一个耗时操作后回调主线程，主线程不需要子线程的处理结果
+    public func execute(waitFor: @escaping () -> (), finshed: @escaping () -> ()) {
         let workItem = DispatchWorkItem {
             waitFor()
         }
@@ -29,5 +45,60 @@ public struct APEvent {
         workItem.wait()
         finshed()
     }
+    
+    /// 串行耗时操作，每一段子任务依赖上一个任务完成，全部完成后回调主线程
+    /// 向全局并发队列添加任务，添加的任务会同步执行
+    public func serial(_ block: @escaping APEventBlock) -> APEvent {
+        var result = self
+        result.blocks.append(block)
+        return result
+    }
+    /// 串行耗时操作，每一段子任务依赖上一个任务完成，全部完成后回调主线程
+    /// 处理完毕的回调，在主线程异步执行
+    public func serialDone(_ block: @escaping APEventBlock) {
+        queue.async {
+            for workItem in self.blocks {
+                workItem()
+            }
+            DispatchQueue.main.async {
+                block()
+            }
+        }
+    }
 
+    /// 并发耗时操作，每一段子任务独立，所有子任务完成后回调主线程
+    /// 向自定义并发队列添加任务，添加的任务会并发执行
+    public func concurrent(_ block: @escaping APEventBlock) -> APEvent {
+        var result = self
+        if result.group == nil {
+            result.group = DispatchGroup()
+        }
+        result.queue = DispatchQueue(label: "apevent.applepie.com", attributes: .concurrent)
+        let workItem = DispatchWorkItem {
+            block()
+        }
+        result.queue.async(group: result.group!, execute: workItem)
+        return result
+    }
+    /// 并发耗时操作，每一段子任务独立，所有子任务完成后回调主线程
+    /// 此任务执行时会排斥其他的并发任务，一般用于写入事务，保证线程安全。
+    public func concurrentBarrier(_ block: @escaping APEventBlock) -> APEvent {
+        var result = self
+        if result.group == nil {
+            result.group = DispatchGroup()
+        }
+        result.queue = DispatchQueue(label: "apevent.applepie.com", attributes: .concurrent)
+        let workItem = DispatchWorkItem(flags: .barrier) {
+            block()
+        }
+        result.queue.async(group: result.group!, execute: workItem)
+        return result
+    }
+    /// 并发耗时操作，每一段子任务独立，所有子任务完成后回调主线程
+    /// 处理完毕的回调，在主线程异步执行
+    public func concurrentDone(_ block: @escaping APEventBlock) {
+        group?.notify(queue: .main, execute: {
+            block()
+        })
+    }
 }
--- ApplepieDemo/ApplepieTests/Data/GCD/APEventTests.swift
+++ ApplepieDemo/ApplepieTests/Data/GCD/APEventTests.swift
@@ -23,7 +23,7 @@ class APEventTests: BaseTestCase {
     func testExecute() {
         let expectation = XCTestExpectation(description: "Complete")
         var color = UIColor.white
-        APEvent.execute(waitFor: {
+        APEvent().execute(waitFor: {
             assert(Thread.current != Thread.main)
             color = UIColor.red
             usleep(100000) // 0.1s
@@ -38,7 +38,7 @@ class APEventTests: BaseTestCase {
 
     func testExecuteWithT() {
         let expectation = XCTestExpectation(description: "Complete")
-        APEvent.execute(waitFor: {
+        APEvent().execute(waitFor: {
             assert(Thread.current != Thread.main)
             let color = UIColor.red
             usleep(100000) // 0.1s
@@ -52,4 +52,64 @@ class APEventTests: BaseTestCase {
         
     }
 
+    func testSerial() {
+        let expectation = XCTestExpectation(description: "Complete")
+        var number = 0
+        APEvent()
+            .serial {
+                assert(Thread.current != Thread.main)
+                number += 1
+                assert(number == 1)
+            }.serial {
+                assert(Thread.current != Thread.main)
+                number += 2
+                assert(number == 3)
+            }.serial {
+                assert(Thread.current != Thread.main)
+                number += 3
+                assert(number == 6)
+            }.serial {
+                assert(Thread.current != Thread.main)
+                number += 4
+                assert(number == 10)
+            }.serial {
+                assert(Thread.current != Thread.main)
+                number += 5
+                assert(number == 15)
+            }.serialDone {
+                assert(Thread.current == Thread.main)
+                assert(number == 15)
+                expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 10)
+    }
+    
+    func testConcurrent() {
+        let expectation = XCTestExpectation(description: "Complete")
+        var number = 0
+        APEvent()
+            .concurrent {
+                assert(Thread.current != Thread.main)
+                number += 1
+            }.concurrent {
+                assert(Thread.current != Thread.main)
+                number += 2
+            }.concurrentBarrier {
+                assert(Thread.current != Thread.main)
+                number += 3
+            }.concurrentBarrier {
+                assert(Thread.current != Thread.main)
+                number += 4
+            }.concurrent {
+                assert(Thread.current != Thread.main)
+                number += 5
+            }.concurrentDone {
+                assert(Thread.current == Thread.main)
+                assert(number == 15)
+                expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 10)
+    }
 }
