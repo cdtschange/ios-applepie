ee2173b4db828f5f3c43ed59c1237f7f4ced9d9c
wei.mao <wei.mao@pintec.com>
2018-12-05 18:59:57 +0800

    fix

--- ApplepieDemo/ApplepieDemo/ViewController.swift
+++ ApplepieDemo/ApplepieDemo/ViewController.swift
@@ -7,16 +7,13 @@
 //
 
 import UIKit
-import PromiseKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        Promise.pending()
     }
-
 
 }
 
